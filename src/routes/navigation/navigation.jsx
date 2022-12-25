230a47f096b1e88ad24255982a786e67580451c5
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2022-12-25 17:17:17 -0300

    Navbar and beginning of login

---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom"
+import { Fragment } from "react"
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import './navigation.scss'
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation">
+          <Link className="logo-container" to='/'>  {/* tag Link vai mandar pro path especificado */}
+            <CrownLogo className="logo" />
+          </Link>
+            <div className="links-container">
+                <Link className="link" to='/shop'>
+                    SHOP
+                </Link>
+                <Link className="link" to='/sign-in'>
+                    SIGN IN
+                </Link>
+            </div>
+        </div>
+        <Outlet /> 
+      </Fragment>
+    )
+}
+
+export default Navigation
