7efb5c49d5620c8963588a7ab503e4688a37a3db
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2023-01-13 17:38:05 -0300

    Fixing style bug 🛠️

--- src/components/category-item/category-item.jsx
+++ src/components/category-item/category-item.jsx
@@ -4,12 +4,12 @@ const CategoryItem = ({ category }) => {
     const { imageUrl, title } = category
 
     return (
-        <div className="category-container">
+        <div className="category-item-container">
           <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`
           }} 
           />
-          <div className="category-body-container">
+          <div className="body">
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
